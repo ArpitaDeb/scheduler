d6301f9cc1a17e5e17d422428b59812f9965d425
ArpitaDeb <arpitadeb1990@gmail.com>
2020-07-11 23:54:34 -0400

    handling transition error modes while saving/deleting error occurs

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -7,9 +7,10 @@ import useVisualMode from "../../hooks/useVisualMode";
 import Form from "./Form";
 import Status from "./Status";
 import Confirm from "./Confirm";
+import Error from "./Error";
 
 export default function Appointment(props) {
-  console.log("index",props);
+  console.log("index", props);
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
@@ -17,6 +18,8 @@ export default function Appointment(props) {
   const DELETING = "DELETING";
   const CONFIRM = "CONFIRM";
   const EDIT = "EDIT";
+  const ERROR_SAVE = "ERROR_SAVE";
+  const ERROR_DELETE = "ERROR_DELETE";
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
@@ -27,14 +30,18 @@ export default function Appointment(props) {
       interviewer: interviewer
     };
     transition(SAVING);
-    props.bookInterview(props.id, interview)
-      .then(() => transition(SHOW));
-    //transition(SHOW);
+    props
+      .bookInterview(props.id, interview)
+      .then(() => transition(SHOW))
+      .catch((error) => transition(ERROR_SAVE, true));
+    //transition(SHOW)
   }
-  function deleteInterviewApt(id) {
-    transition(DELETING);
-    props.cancelInterview(id)
-      .then(() => transition(EMPTY));
+  function destroy(event) {
+    transition(DELETING, true);
+    props
+      .cancelInterview(props.id)
+      .then(() => transition(EMPTY))
+      .catch(() => transition(ERROR_DELETE, true))
   }
 
   return (
@@ -52,8 +59,10 @@ export default function Appointment(props) {
       {mode === CREATE && <Form interviewers={props.interviewers} onSave={save} onCancel={back} />}
       {mode === SAVING && <Status message="saving" />}
       {mode === DELETING && <Status message="Deleting" />}
-      {mode === CONFIRM && <Confirm message="Are you sure?" onCancel={back} onConfirm={() => deleteInterviewApt(props.id)} />}
+      {mode === CONFIRM && <Confirm message="Are you sure?" onCancel={back} onConfirm={() => destroy(props.id)} />}
       {mode === EDIT && <Form name={props.interview.student} interviewer={props.interview.interviewer.id} interviewers={props.interviewers} onSave={save} onCancel={back} />}
+      {mode === ERROR_SAVE && <Error message="unable to save appointment" onClose={back} />}
+      {mode === ERROR_DELETE && <Error message="could not cancel to appointment" onClose={back} />}
     </article>
   );
 }
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,14 +3,18 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
   const transition = (newmode, replace = false) => {
+    console.log('checking mode setting', newmode);
     setMode(newmode);
     if (replace) {
       const newHistory = [...history];
       newHistory.pop();
       newHistory.push(newmode);
       setHistory(newHistory);
+      console.log(history);
     } else {
-      setHistory([...history, newmode]);
+      //setHistory([...history, newmode]);
+      setHistory(prev => ([...prev, newmode]));
+      console.log(history);
     }
   }
   const back = () => {
@@ -18,6 +22,7 @@ export default function useVisualMode(initial) {
       const updatedHistory = [...history];
       updatedHistory.pop();
       setHistory(updatedHistory);
+      console.log(updatedHistory);
       setMode(updatedHistory[updatedHistory.length - 1]);
     }
   }
