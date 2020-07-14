303ca7c3e355c259dab04db3e226b6dda65e3571
ArpitaDeb <arpitadeb1990@gmail.com>
2020-07-14 19:50:32 -0400

    easier async await promise chain

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -9,11 +9,30 @@ afterEach(cleanup);
 xit("renders without crashing", () => {
   render(<Application />);
 });
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
+xit("defaults to Monday and changes the schedule when a new day is selected", () => {
   const { getByText } = render(<Application />);
 
   return waitForElement(() => getByText("Monday")).then(() => {
     fireEvent.click(getByText("Tuesday"));
     expect(getByText("Leopold Silvers")).toBeInTheDocument();
   });
+  
+});
+xit("changes the schedule when a new day is selected", () => {
+  const { getByText } = render(<Application />);
+
+  return waitForElement(() => getByText("Monday")).then(() => {
+    fireEvent.click(getByText("Tuesday"));
+
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
+});
+it("changes the schedule when a new day is selected", async () => {
+  const { getByText } = render(<Application />);
+
+  await waitForElement(() => getByText("Monday"));
+
+  fireEvent.click(getByText("Tuesday"));
+
+  expect(getByText("Leopold Silvers")).toBeInTheDocument();
 });
