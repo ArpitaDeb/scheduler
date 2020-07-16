c20d59231aed4181ee216fb0a258366e5d3e5e89
ArpitaDeb <arpitadeb1990@gmail.com>
2020-07-16 16:38:27 -0400

    writing cypress appointment tests

--- src/components/Appointment/Empty.js
+++ src/components/Appointment/Empty.js
@@ -13,3 +13,10 @@ export default function Empty(props) {
     </main>
   );
 }
+/*
+Timed out retrying: cy.click() failed because this element is not visible:
+
+<img class="appointment__add-button" src="images/add.png" alt="Add">
+
+This element <img.appointment__add-button> is not visible because its parent <main.appointment__add> has CSS property: display: none
+*/
