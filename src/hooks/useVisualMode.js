a03089971b4caca968086660b5268b8b1c620913
ARPITA DEB <40507760+ArpitaDeb@users.noreply.github.com>
2020-07-19 00:22:40 -0400

    Update useVisualMode.js

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,8 +1,12 @@
 import { useState } from "react";
+
+//Custom hooks to track transition modes history of the appointment component
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
+  //if replace is true, replace last mode with the input newmode 
+  // if replace is false, push the new mode
   const transition = (newmode, replace = false) => {
     setMode(newmode);
     if (replace) {
@@ -11,7 +15,8 @@ export default function useVisualMode(initial) {
       setHistory(prev => ([...prev, newmode]));
     }
   }
-
+  
+//delete the last mode and set updated last mode 
   const back = () => {
     if (history.length > 1) {
       const updatedHistory = [...history];
@@ -20,5 +25,6 @@ export default function useVisualMode(initial) {
       setMode(updatedHistory[updatedHistory.length - 1]);
     }
   }
+  //return these functions to update mode transition in other components
   return { mode, transition, back };
 }
